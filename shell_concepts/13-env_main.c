42f33eeee263c1bf173bb99b324649ea42bd12f7
Marvel957 <hamsik957@gmail.com>
2022-08-10 02:15:49 -0700

    edit on 13

--- shell_concepts/13-env_main.c
+++ shell_concepts/13-env_main.c
@@ -3,19 +3,22 @@
 /**
  * main - prints the environment
  *
+ * @ac: func param
+ * @av: func param
+ * @env: func param
  * Return: Always 0.
  */
 int main(int ac, char **av, char **env)
 {
-    unsigned int i;
-	
-    i = 0;
-    (void)ac;
-    (void)av;
-    while (env[i] != NULL)
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
-    return (0);
+	unsigned int i;
+
+	i = 0;
+	(void)ac;
+	(void)av;
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	return (0);
 }
